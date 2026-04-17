bf3b84f10fbf0921e98b3b11aca4851acf09d9b2
Jonas Barth <j.barth@muv.com>
2026-04-17 14:52:35 +0200

    Projektstruktur überarbeitet und Spatie Package Tools entfernt

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
-use Illuminate\Support\Facades\Route;
-use muv\LaravelDeployment\Http\Controllers\DeployController;
-
-// Call this route with a GitHub webhook for push events
-Route::post('/git-deploy', DeployController::class)->withoutMiddleware(ValidateCsrfToken::class);
